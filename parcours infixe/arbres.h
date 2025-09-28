32cdb4029dcdcc0d7b25d178f33852e272ea8031
nitroo4 <mickarzk@gmail.com>
2025-09-28 11:39:56 +0300

    parcours_infixe

--- parcours infixe/arbres.h
+++ parcours infixe/arbres.h
@@ -1,22 +1,17 @@
 #ifndef ARBRES_H_INCLUDED
 #define ARBRES_H_INCLUDED
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
 #define ELEMENTNULL 0
 #define ARBRENULL NULL
 #define MALLOC(x) ((x*) malloc(sizeof(x)))
-
 typedef int Element;
-
 typedef struct noeud {
     Element elmt;
-    struct noeud* racine;   // premier fils
-    struct noeud* suivant;  // frère suivant
+    struct noeud* racine;   // fils
+    struct noeud* suivant;  // suivant
 } Noeud;
-
 typedef Noeud* PArbre;
 
 /* Prototypes */
